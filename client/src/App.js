41ee440c9b811b57ff0e2850b8271872ca2a6be3
Slava Kim <slava@ncsoft.com>
2019-01-29 14:40:39 +0900

    label selection ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,75 @@
 import React, { Component } from 'react';
-import { Container, Divider, Grid, Header, Image } from 'semantic-ui-react';
+import { Grid, Container, Header, List, Label } from 'semantic-ui-react';
 
 import 'semantic-ui-css/semantic.min.css';
 
 import Canvas from './Canvas';
 import './App.css';
 
+const shortcuts = '1234567890qwe';
+const colors = [
+  'red',
+  'blue',
+  'green',
+  'violet',
+  'orange',
+  'brown',
+  'yellow',
+  'olive',
+  'teal',
+  'purple',
+  'pink',
+  'grey',
+  'black',
+];
+
+function ListItem({ shortcut, label, onSelect, color, selected = false }) {
+  return (
+    <List.Item onClick={onSelect} active={selected}>
+      <Label color={color} horizontal>
+        {shortcut}
+      </Label>{' '}
+      {label}
+    </List.Item>
+  );
+}
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selected: null,
+    };
+  }
   render() {
+    const labels = ['cat', 'dog', 'car', 'tree', 'another one'];
     return (
       <div>
         <Grid columns={3} divided stretched>
           <Grid.Row stretched>
-            <Grid.Column width={4}>text 1</Grid.Column>
-            <Grid.Column width={8}>
+            <Grid.Column width={4} style={{ overflow: 'auto' }}>
+              <div style={{ padding: '1em 0' }}>
+                <Header size="large" align="center">
+                  Labels
+                </Header>
+                <List divided selection>
+                  {labels.map((label, i) =>
+                    ListItem({
+                      shortcut: shortcuts[i],
+                      label,
+                      color: colors[i],
+                      onSelect: () => {
+                        this.setState({ selected: label });
+                      },
+                      selected: this.state.selected === label,
+                    })
+                  )}
+                </List>
+              </div>
+            </Grid.Column>
+            <Grid.Column width={12} style={{ padding: 0 }}>
               <Canvas url="http://kempe.net/images/newspaper-big.jpg" />
             </Grid.Column>
-            <Grid.Column width={4}>text 3</Grid.Column>
           </Grid.Row>
         </Grid>
       </div>
